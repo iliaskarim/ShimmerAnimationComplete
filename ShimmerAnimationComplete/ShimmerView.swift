079ff7135f7bc3c6e1e378bf10a98cef14507019
Ilias Karim <ilias.karim@gmail.com>
2022-08-31 11:50:47 -0400

    Add stopAnimating method

--- ShimmerAnimationComplete/ShimmerView.swift
+++ ShimmerAnimationComplete/ShimmerView.swift
@@ -8,43 +8,39 @@
 
 import UIKit
 
-class ShimmerView: UIView {
+/// Shimmer view
+final class ShimmerView: UIView {
+  private var animation: CABasicAnimation = {
+    let animation = CABasicAnimation(keyPath: "locations")
+    animation.fromValue = [-1.0, -0.5, 0.0]
+    animation.toValue = [1.0, 1.5, 2.0]
+    animation.repeatCount = .infinity
+    animation.duration = 0.9
+    return animation
+  }()
 
-    var gradientColorOne : CGColor = UIColor(white: 0.85, alpha: 1.0).cgColor
-    var gradientColorTwo : CGColor = UIColor(white: 0.95, alpha: 1.0).cgColor
-    
-    
-    
-    func addGradientLayer() -> CAGradientLayer {
-        
-        let gradientLayer = CAGradientLayer()
-        
-        gradientLayer.frame = self.bounds
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
-        gradientLayer.colors = [gradientColorOne, gradientColorTwo, gradientColorOne]
-        gradientLayer.locations = [0.0, 0.5, 1.0]
-        self.layer.addSublayer(gradientLayer)
-        
-        return gradientLayer
-    }
-    
-    func addAnimation() -> CABasicAnimation {
-       
-        let animation = CABasicAnimation(keyPath: "locations")
-        animation.fromValue = [-1.0, -0.5, 0.0]
-        animation.toValue = [1.0, 1.5, 2.0]
-        animation.repeatCount = .infinity
-        animation.duration = 0.9
-        return animation
-    }
-    
-    func startAnimating() {
-        
-        let gradientLayer = addGradientLayer()
-        let animation = addAnimation()
-       
-        gradientLayer.add(animation, forKey: animation.keyPath)
-    }
+  private lazy var gradientLayer: CAGradientLayer = {
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.frame = bounds
+    gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+    gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+    gradientLayer.colors = [
+      UIColor(white: 0.75, alpha: 1.0).cgColor,
+      UIColor(white: 0.85, alpha: 1.0).cgColor,
+      UIColor(white: 0.75, alpha: 1.0).cgColor]
+    gradientLayer.locations = [0.0, 0.5, 1.0]
+    return gradientLayer
+  }()
 
+  /// Begin animation
+  func startAnimating() {
+    layer.addSublayer(gradientLayer)
+    gradientLayer.add(animation, forKey: animation.keyPath)
+  }
+
+  /// End animation
+  func stopAnimating(){
+    layer.removeAllAnimations()
+    gradientLayer.removeFromSuperlayer()
+   }
 }
